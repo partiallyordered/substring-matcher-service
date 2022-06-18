bafa14935dd8466a3c9b5babdafd11939355be96
Matt Kingston <mattkingston@protonmail.com>
2022-06-18 21:13:12 +0200

    Don't print Spring banner on boot- we want JSON only

--- src/main/java/com/example/substring_matcher_service/StringMatcherServiceApplication.java
+++ src/main/java/com/example/substring_matcher_service/StringMatcherServiceApplication.java
@@ -2,12 +2,15 @@ package com.example.substring_matcher_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.Banner;
 
 @SpringBootApplication
 public class StringMatcherServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StringMatcherServiceApplication.class, args);
+        SpringApplication app = new SpringApplication(StringMatcherServiceApplication.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
     }
 
 }
